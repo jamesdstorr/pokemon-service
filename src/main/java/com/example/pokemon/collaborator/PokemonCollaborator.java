b35eea4811bde28fb785cde7182242ba96b901f6
James Storr <jamesstorr@live.co.uk>
2024-05-03 13:42:23 +0100

    fixed 500 errors when trying to evolve pokemon in last evolution stage

--- src/main/java/com/example/pokemon/collaborator/PokemonCollaborator.java
+++ src/main/java/com/example/pokemon/collaborator/PokemonCollaborator.java
@@ -8,39 +8,34 @@ import com.example.pokemon.model.PokemonSpecies;
 import com.example.pokemon.model.Evolution.EvolutionPokemonSpecies;
 import com.example.pokemon.model.Evolution.EvolutionResponse;
 import com.example.pokemon.model.Evolution.EvolvesTo;
-import com.fasterxml.jackson.databind.ObjectMapper;
-
-
-
-
 
 
 @Service
 public class PokemonCollaborator {
 
-    public EvolutionPokemonSpecies getNextEvolution(String pokemonName, EvolvesTo evolvesTo ){
+    public EvolutionPokemonSpecies getNextEvolution(String pokemonName, EvolvesTo evolvesTo) {
 
-        if(evolvesTo.getSpecies().getName().equalsIgnoreCase(pokemonName)){
-            if(!evolvesTo.getEvolves_to().isEmpty()){
+        if (evolvesTo.getSpecies() != null && evolvesTo.getSpecies().getName().equalsIgnoreCase(pokemonName)) {
+            if (!evolvesTo.getEvolves_to().isEmpty()) {
                 return evolvesTo.getEvolves_to().get(0).getSpecies();
             }
             return null;
-        }
-        else{
-            for(EvolvesTo nextEvolvesTo : evolvesTo.getEvolves_to()){
-                EvolutionPokemonSpecies pokemonSpecies = getNextEvolution(pokemonName, nextEvolvesTo );
-                if (pokemonSpecies != null) {
-                    return pokemonSpecies;
+        } else {
+            for (EvolvesTo nextEvolvesTo : evolvesTo.getEvolves_to()) {
+                System.out.println(nextEvolvesTo);
+                if(!nextEvolvesTo.getEvolves_to().isEmpty()){
+                    EvolutionPokemonSpecies pokemonSpecies = getNextEvolution(pokemonName, nextEvolvesTo);
+                    if (pokemonSpecies!= null &&!pokemonSpecies.getName().isEmpty()) {
+                        return pokemonSpecies;
+                    }
                 }
+                else return null;
             }
         }
         return null;
     }
 
-
-
     private final RestTemplate restTemplate = new RestTemplate();
-    private static final ObjectMapper objectMapper = new ObjectMapper();
 
     public Pokemon getPokemonByNameOrID(String idOrName) {
         String url = "https://pokeapi.co/api/v2/pokemon/" + idOrName;
@@ -53,24 +48,30 @@ public class PokemonCollaborator {
                 .getForObject("https://pokeapi.co/api/v2/pokemon-species/" + idOrName, PokemonSpecies.class);
 
         final String pokemonName = pokemonSpecies.getName();
- 
         EvolutionResponse evolutionResponse = restTemplate.getForObject(pokemonSpecies.getEvolution_chain().getUrl(),
                 EvolutionResponse.class);
 
-        if(evolutionResponse!=null && evolutionResponse.getChain().getSpecies().getName().equals(pokemonName)){
+        if (evolutionResponse != null && evolutionResponse.getChain().getSpecies().getName().equals(pokemonName)) {
             EvolutionPokemonSpecies nextEvolution = evolutionResponse.getChain().getEvolves_to().get(0).getSpecies();
             System.out.println(nextEvolution.getName());
             return getPokemonByNameOrID(nextEvolution.getName());
-        }
-        else {
+        } else {
             EvolvesTo evolvesTo = evolutionResponse.getChain().getEvolves_to().get(0);
-            String nextEvolution = getNextEvolution(pokemonName, evolvesTo).getName();
-            System.out.println(nextEvolution);
-            return getPokemonByNameOrID(nextEvolution);
+            if (evolvesTo != null && evolvesTo.getSpecies().getName() != null ) {
+                EvolutionPokemonSpecies nextEvolution = getNextEvolution(pokemonName, evolvesTo);
+                if (nextEvolution != null){
+                    return getPokemonByNameOrID(nextEvolution.getName());
+                }
+                System.out.println("No evolve");
+                return null;
+            }
+            else{
+                System.out.println("No evolve");
+                return null;
+            }
+
         }
 
     }
 
 }
-
-
--- src/main/java/com/example/pokemon/controller/PokemonController.java
+++ src/main/java/com/example/pokemon/controller/PokemonController.java
@@ -1,6 +1,7 @@
 package com.example.pokemon.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,12 +10,25 @@ import org.springframework.web.bind.annotation.PostMapping;
 
 import com.example.pokemon.model.Pokemon;
 import com.example.pokemon.service.PokemonService;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+
+
 
 
 @RestController // Indicates that this class is a REST controller, which means it handles HTTP requests and returns responses.
 public class PokemonController {
+    
     @Autowired
     private PokemonService pokemonService; // Injects an instance of PokemonService into this controller, making it available for use in methods like getPokemonByName().
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    public PokemonController(ObjectMapper objectMapper) {
+        this.objectMapper = objectMapper;
+        this.objectMapper.enable(SerializationFeature.INDENT_OUTPUT);
+    }
 
     @GetMapping("/pokemon/{name}") // Maps incoming HTTP GET requests with a URI matching /pokemon/{name} to the getPokemonByName() method.
     public ResponseEntity<Pokemon> getPokemonByName(@PathVariable("name") String name){ // Handles HTTP GET requests and returns a ResponseEntity containing a Pokemon object. 
@@ -25,7 +39,17 @@ public class PokemonController {
    @PostMapping("/pokemon/evolve/{name}")
     public ResponseEntity<String> getPokemonEvolution(@PathVariable("name") String name) {
         Pokemon pokemon = pokemonService.evolvePokemon(name); // Declares a local variable pokemon of type Pokemon within the getPokemonByName() method.
-        return ResponseEntity.ok("An evolved Pokemon");
+        if (pokemon == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).body("Evolution for " + name + " not found.");
+        } else {
+            try {
+                String pokemonJson = objectMapper.writeValueAsString(pokemon);
+                return ResponseEntity.ok(pokemonJson);
+            } catch (JsonProcessingException e) {
+                // Handle JSON processing exception
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error processing Pokémon data.");
+            }
+        }
     }
 
 }
